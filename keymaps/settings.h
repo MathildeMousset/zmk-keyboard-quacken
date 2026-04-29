fb3387220588020c5c1b6c169180fe8374246c3b
MathildeMousset <mathilde.mousset@gmail.com>
2026-04-29 15:01:22 +0200

    add option

--- keymaps/settings.h
+++ keymaps/settings.h
@@ -35,7 +35,7 @@
 // Uncomment the following line for an improved dead key support;
 // (only applies to some Hummingbird keymaps and layout emulations)
 
-// #define ENABLE_FANCY_DEAD_KEYS
+#define ENABLE_FANCY_DEAD_KEYS
 
 
 /******************************************************************************
